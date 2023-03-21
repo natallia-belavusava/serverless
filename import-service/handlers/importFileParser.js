3c711f6039b840ed1b3fa63cbc073ece0a107f31
Natallia_Belavusava <Natallia_Belavusava@epam.com>
2023-03-21 17:41:43 +0100

    fix csv files parsing order

--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -16,11 +16,27 @@ const sendToSQS = (data) => {
 const importedFileTransformer = async (event) => {
   try {
     const s3 = new S3({ region: process.env.REGION });
-    const results = [];
+
     for (const record of event.Records) {
       const Bucket = record.s3.bucket.name;
       const Key = record.s3.object.key;
       const newKey = record.s3.object.key.replace("uploaded", "parsed");
+
+      const params = { Bucket, Key };
+      const stream = await s3.getObject(params);
+
+      stream.Body.pipe(csv()).on("data", (data) => {
+        const obj = {};
+        for (let key in data) {
+          const values = data[key].split(";");
+          const keys = key.split(";");
+          keys.forEach((k, index) => {
+            obj[k] = values[index];
+          });
+        }
+        sendToSQS(JSON.stringify(obj));
+      });
+
       await s3.copyObject({
         Bucket,
         Key: newKey,
@@ -29,25 +45,8 @@ const importedFileTransformer = async (event) => {
 
       await s3.deleteObject({
         Bucket,
-        Key: Key,
+        Key,
       });
-
-      const params = { Bucket, Key: newKey };
-      const stream = await s3.getObject(params);
-
-      stream.Body.pipe(csv())
-        .on("data", (data) => {
-          const obj = {};
-          for (let key in data) {
-            const values = data[key].split(";");
-            const keys = key.split(";");
-            keys.forEach((k, index) => {
-              obj[k] = values[index];
-            });
-          }
-          sendToSQS(JSON.stringify(obj));
-        })
-
     }
 
     return {
@@ -55,7 +54,7 @@ const importedFileTransformer = async (event) => {
       headers: {
         "Access-Control-Allow-Origin": "*",
       },
-      body: JSON.stringify(results),
+      body: "file parsed",
     };
   } catch (error) {
     return {
